7a7e6781c7a76c20715a2b996ba07b01a8ad03fc
remiangelo <justryeoi@gmail.com>
2025-08-10 11:55:19 +0900

    Im going to fucking castrate myself and you andre.

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
   expo: {
     name: 'Harvest',
-    slug: 'harvest-app',
+    slug: 'harvest',
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',

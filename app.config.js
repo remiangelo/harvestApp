64770dd08508752408571ed386670417baaee937
remiangelo <justryeoi@gmail.com>
2025-09-14 12:39:21 +0900

    Build number updated

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: 'com.harvest.harvestdating',
-      buildNumber: '10',
+      buildNumber: '11',
       appleTeamId: 'L3P46Q9398',
       config: {
         usesNonExemptEncryption: false,

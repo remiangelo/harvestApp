0250575e967d2f6832fa363d62672c0224e40bf8
Andre <a.giron@pitturagroup.com>
2025-11-21 02:31:16 +0400

    Forgot to update build n

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: 'Harvest',
     slug: 'harvest',
-    version: '1.4.8',
+    version: '1.4.9',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'harvestapp',
@@ -16,7 +16,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: 'com.harvest.harvestdating',
-      buildNumber: '37',
+      buildNumber: '38',
       appleTeamId: 'L3P46Q9398',
       config: {
         usesNonExemptEncryption: false,

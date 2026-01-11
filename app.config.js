6dd0a82efe055f11ab38dd229f883fcc39ac28f8
Andre <a.giron@pitturagroup.com>
2026-01-11 19:08:18 +0400

    Build n

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: 'com.harvest.harvestdating',
-      buildNumber: '54',
+      buildNumber: '55',
       appleTeamId: 'L3P46Q9398',
       config: {
         usesNonExemptEncryption: false,
